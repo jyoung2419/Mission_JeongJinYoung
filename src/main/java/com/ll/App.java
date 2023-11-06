4a023622c3cc89da59354403cfea82de9922a97a
Jeong jinyoung <jjyoung2419@gmail.com>
2023-11-06 22:31:16 +0900

    step1

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,21 @@
+package com.ll;
+
+
+import java.util.Scanner;
+
+class App {
+    void run() {
+        while (true) {
+            System.out.println("== 명언 앱 ==");
+            System.out.print("명령) ");
+
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("종료")){
+                break;
+            }
+            System.out.printf("입력받은 명령어 : %s\n", cmd);
+        }
+    }
+}
