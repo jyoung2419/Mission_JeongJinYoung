666dbaea1a633c8b8fdc2cde8e43bafb541be4b1
Jeong jinyoung <jjyoung2419@gmail.com>
2023-11-08 02:43:50 +0900

    step3

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -7,21 +7,27 @@ class App {
     void run() {
         System.out.println("== 명언 앱 ==");
 
+        int contentId = 0;
+
         while (true) {
             System.out.print("명령) ");
 
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
 
-            if (cmd.equals("종료")){
+            if (cmd.equals("종료")) {
                 break;
-            }
-            else if (cmd.equals("등록")){
+            } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
                 String content = scanner.nextLine();
                 System.out.print("작가 : ");
                 String authorName = scanner.nextLine();
+
+                contentId++;
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", contentId);
             }
         }
+
     }
 }
