2b0f24e35ba56d57bd0d7c3ae316b0a0b0db9573
Jeong jinyoung <jjyoung2419@gmail.com>
2023-11-08 11:14:25 +0900

    step7 rq 클래스 수정, step8

--- src/main/java/com/ll/Quotation.java
+++ src/main/java/com/ll/Quotation.java
@@ -1,12 +1,20 @@
 package com.ll;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Quotation {
-    int id;
+    @Getter
+    int contentId;
+    @Getter
+    @Setter
     String content;
+    @Getter
+    @Setter
     String authorName;
 
-    public Quotation(int id, String content, String authorName) {
-        this.id = id;
+    public Quotation(int contentId, String content, String authorName) {
+        this.contentId = contentId;
         this.content = content;
         this.authorName = authorName;
     }
